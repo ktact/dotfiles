1ca07e531e1e942c4b35df24307b0591b370b279
Kadohata Tact <ktact.dev@gmail.com>
2017-02-11 14:44:52 +0900

    add directories under .vim

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+ln -s $HOME/dotfiles/.vim/colors $HOME/.vim/colors
+ln -s $HOME/dotfiles/.vim/indent $HOME/.vim/indent
