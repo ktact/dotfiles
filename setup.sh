57b819c4486c57e624178eccd8e4e7d3b5a8e19b
K Tact <ktact.dev@gmail.com>
2021-05-06 20:14:55 +0900

    git-prompt導入

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ done
 
 ln -sfnv $HOME/dotfiles/.vim/colors $HOME/.vim/colors
 ln -sfnv $HOME/dotfiles/.vim/indent $HOME/.vim/indent
+
+echo "source ~/.git-prompt.sh" >> ~/.bashrc
+echo "export PS1='\[\033[1;32m\]\u\[\033[00m\]:\[\033[1;34m\]\w\[\033[1;31m\]$(__git_ps1)\[\033[00m\] \$ '" >> ~/.bash_profile
