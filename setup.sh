8bf3c4a1ad1dc044aeeb69f80db8cfc3a30aea5b
K Tact <ktact.dev@gmail.com>
2021-05-17 21:28:34 +0900

    npmライブラリのインストール処理を追加

--- setup.sh
+++ setup.sh
@@ -13,3 +13,15 @@ ln -sfnv $HOME/dotfiles/.vim/indent $HOME/.vim/indent
 
 echo "source ~/.git-prompt.sh" >> ~/.bashrc
 echo "export PS1='\[\033[1;32m\]\u\[\033[00m\]:\[\033[1;34m\]\w\[\033[1;31m\]$(__git_ps1)\[\033[00m\] \$ '" >> ~/.bash_profile
+
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
+if has "npm"; then
+  echo "Install npm packages..."
+
+  sudo npm i -g git-split-diffs
+
+  echo "$(tput setaf 2)Install npm packages complete. ✔︎$(tput sgr0)"
+fi
