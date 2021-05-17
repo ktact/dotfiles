6e19eb350fa34e55c528e3b1606215a8b6dcedd9
K Tact <ktact.dev@gmail.com>
2021-05-17 21:32:05 +0900

    ドットファイルのデプロイ処理を関数化

--- setup.sh
+++ setup.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-for file in .??*
-do
-  [ "$file" = ".git" ] && continue
-  [ "$file" = ".gitignore" ] && continue
+link_files() {
+  for file in .??*
+  do
+    [ "$file" = ".git" ] && continue
+    [ "$file" = ".gitignore" ] && continue
 
-  ln -sfnv $HOME/dotfiles/$file $HOME/$file
-done
+    ln -sfnv $HOME/dotfiles/$file $HOME/$file
+  done
 
-ln -sfnv $HOME/dotfiles/.vim/colors $HOME/.vim/colors
-ln -sfnv $HOME/dotfiles/.vim/indent $HOME/.vim/indent
+  ln -sfnv $HOME/dotfiles/.vim/colors $HOME/.vim/colors
+  ln -sfnv $HOME/dotfiles/.vim/indent $HOME/.vim/indent
 
-echo "source ~/.git-prompt.sh" >> ~/.bashrc
-echo "export PS1='\[\033[1;32m\]\u\[\033[00m\]:\[\033[1;34m\]\w\[\033[1;31m\]$(__git_ps1)\[\033[00m\] \$ '" >> ~/.bash_profile
+  echo "$(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)"
+}
 
 has() {
   type "$1" > /dev/null 2>&1
 }
+
+link_files
+
+echo "source ~/.git-prompt.sh" >> ~/.bashrc
+echo "export PS1='\[\033[1;32m\]\u\[\033[00m\]:\[\033[1;34m\]\w\[\033[1;31m\]$(__git_ps1)\[\033[00m\] \$ '" >> ~/.bash_profile
 
 if has "npm"; then
   echo "Install npm packages..."
