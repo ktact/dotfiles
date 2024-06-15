e2a6ee8e17590cd9c622fcce10c1fef68519c3fb
ktact <ktact.dev@gmail.com>
2024-06-15 17:19:38 +0900

    Update docker installation instructions

--- packages.sh
+++ packages.sh
@@ -9,16 +9,17 @@ function install_docker() {
     sudo apt-get update > /dev/null
     sudo apt-get install \
       ca-certificates \
-      curl \
-      gnupg > /dev/null
+      curl > /dev/null
     ## 2. Add Docker's official GPG key:
-    sudo mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg > /dev/null
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
     ## 3. Set up the repository:
     echo \
-      "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      \"$(. /etc/os-release && echo \"$VERSION_CODENAME\")\" stable" | \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
     # Install Docker Engine
     sudo apt-get update > /dev/null
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
